ac89c5a5c61d26edbae52ac74c7c30cae2b4a1ba
c00p75 <georgecoopmsapenda@gmail.com>
2023-06-09 17:18:45 +0200

    user.rb test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   # Using classname and foreign key because the association cannot be derived from the foreign key name.
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0  }
+  
+  
   # Define associations
   # Rails assumes column used to hold the foreign key on this model is the name of the association with the suffix "_id"
   # Using class_name and foreign key because foreign key is not named as suffix "_id"
