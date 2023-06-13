43b598e5bb89771b114ce4cd07c99c13a81766c8
c00p75 <georgecoopmsapenda@gmail.com>
2023-06-13 11:55:07 +0200

    Add plain html placeholders to views files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = "Here is a list of posts by user."
   end
 
   def show
-   @post = "Here is user's post with id " + params[:id]
   end
 end
