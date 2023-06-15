d5bf48a22a27e74adce5de762552d24a5340e66e
c00p75 <georgecoopmsapenda@gmail.com>
2023-06-15 13:40:13 +0200

    Add view for posts.index

--- app/helpers/users_posts_helper.rb
+++ app/helpers/users_posts_helper.rb
@@ -10,13 +10,17 @@ module UsersPostsHelper
     current_page?(user_path(user)) ? user.name : link_to(user.name, user)
   end
 
-  def posts_list(posts)
+  def posts_list(posts, partial)
     content_tag(:ul) do
-      posts.map { |post| render partial: "users/user_post", locals: { post: post } }.join.html_safe
+      posts.map { |post| render partial: partial, locals: { post: post } }.join.html_safe
     end
   end
 
   def post_link(post)
-    current_page?(user_posts_path(post)) ? post.title : link_to(post.title, user_post_path(post.author_id, post))
+    current_page?(user_post_path(post.author_id, post)) ? post.title : link_to(post.title, user_post_path(post.author_id, post))
+  end
+
+  def comments_list(comments)
+    comments.map { |comment| render partial: "posts/comment", locals: { comment: comment } }.join.html_safe
   end
 end
