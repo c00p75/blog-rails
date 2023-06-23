0bdd20167d69962df40b604f93a9ac3d6546e61c
Caleb Nwaizu <calebchris000@gmail.com>
2023-06-23 13:45:30 +0100

    Complete changes

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,8 @@ Bundler.require(*Rails.groups)
 
 module BlogApp
   class Application < Rails::Application
-    config.api_only = true
-    config.debug_exception_response_format = :api
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
-    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
   root "users#index"
-  namespace :api do
-    get "comments/index"
+
+  resources :users, only: [:index, :show] do
+    resources :posts do
+      resources :comments, only: [:index, :create, :destroy]
+      resources :likes, only: [:create]
+    end
   end
+  
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :show] do
       resources :posts do
@@ -12,4 +17,6 @@ Rails.application.routes.draw do
       end
     end
   end
+
+
 end
