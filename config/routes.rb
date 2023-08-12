86564cfd704eb20adad92a9da84591ef293ebed2
c00p75 <georgecoopmsapenda@gmail.com>
2023-08-12 22:21:11 +0200

    Add test user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
   root "users#index"
+  # Add the route for the test user login
+  devise_scope :user do
+    get '/login_as_test_user', to: 'users/sessions#login_as_test_user', as: :login_as_test_user
+  end
 
   resources :users, only: [:index, :show] do
     resources :posts do
-      resources :comments, only: [:index, :create, :destroy]
+      resources :comments
       resources :likes, only: [:create]
     end
   end
